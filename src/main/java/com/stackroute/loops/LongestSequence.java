bfed9ad4a2b815a0272f0f40932d82d8ce39a041
shahid <shahdbest@gmail.com>
2022-08-11 13:10:53 +0530

    a

---
+++ src/main/java/com/stackroute/loops/LongestSequence.java
@@ -0,0 +1,26 @@
+package com.stackroute.loops;
+
+
+public class LongestSequence {
+    public static void main(String[] args) {
+        new LongestSequence().inputAcceptor();
+    }
+
+    //write logic to get inputs from user and send inputs to inputValidator
+    public void inputAcceptor() {
+    }
+
+    //write logic to get inputs from user and send inputs to findLongestSequence
+    public void inputValidator(int number) {
+    }
+
+    //write logic to longest sequence of zeros in binary representation of given number , returns result
+    public int findLongestSequence(int number) {
+        return (Integer) null;
+    }
+
+    //write logic to print the given printStatement
+    public void outputPrinter(Object printStatement) {
+
+    }
+}
